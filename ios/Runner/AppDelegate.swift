cfdfcbd1b99252abb6c144a53e0258f354da99bf
kazudmb <nakano.kazuki.dmb@gmail.com>
2021-06-08 01:10:43 +0900

    add 今日やった分までをコミット

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,54 @@
 import UIKit
 import Flutter
+import AVFoundation
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    private let methodChannelName = "com.dmb/light_alarm"
+    private let methodTest = "setTorchMode"
+    
+    private var flutterViewController: FlutterViewController {
+        return self.window.rootViewController as! FlutterViewController
     }
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        let methodChannel = FlutterMethodChannel(name: methodChannelName, binaryMessenger: flutterViewController.binaryMessenger)
+        methodChannel.setMethodCallHandler { [weak self] methodCall, result in
+            if methodCall.method == self?.methodTest {
+                let parameters = methodCall.arguments as? String
+                let flg = (parameters == "enabled") ? true : false
+                self?.ledFlash(flg: flg)
+            }
+        }
+        
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+        }
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    private func ledFlash(flg: Bool){
+        let avDevice = AVCaptureDevice.default(for: AVMediaType.video)!
+        if avDevice.hasTorch {
+            do {
+                try avDevice.lockForConfiguration() // torch device lock on
+                if (flg){
+                    avDevice.torchMode = AVCaptureDevice.TorchMode.on
+                } else {
+                    avDevice.torchMode = AVCaptureDevice.TorchMode.off
+                }
+                avDevice.unlockForConfiguration() // torch device unlock
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
 }
